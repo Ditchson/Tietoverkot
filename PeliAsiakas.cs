4115be1683ae9ad6f32d2e811fb3220019335bfe
Ditchson <36441635+Ditchson@users.noreply.github.com>
2018-05-22 13:26:51 +0300

    Add files via upload

---
+++ PeliAsiakas.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+
+namespace PeliAsiakas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket asiakas = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            IPEndPoint iep = new IPEndPoint(IPAddress.Loopback, 9999 );
+            Console.WriteLine("Anna nimi> ");
+            string nimi = Console.ReadLine();
+            string TILA = "CLOSED";
+            string erotin = " ";
+            asiakas.SendTo(Encoding.UTF8.GetBytes("JOIN" + erotin + nimi), iep);
+            TILA = "JOIN";
+            bool on = true;
+
+            while(on)
+            {//while on
+                byte[] rec = new byte[256];
+                IPEndPoint iap = new IPEndPoint(IPAddress.Any, 0);
+                EndPoint remoteEP = (EndPoint)iap;
+                int paljon = asiakas.ReceiveFrom(rec, ref remoteEP);
+
+                string viesti = Encoding.UTF8.GetString(rec, 0, paljon);
+                char[] erottimet = { ' ' };
+                string[] osaset = viesti.Split(erottimet, 3);
+                Console.WriteLine(viesti);
+                switch (TILA)
+                {   //switch TILA
+                    case "JOIN":
+                        switch (osaset[0])
+                        {
+                            case "ACK":
+                                switch(osaset[1])
+                                {
+                                    case "201":
+                                        Console.WriteLine("odotellaan toista pelaajaa...");
+                                        break;
+                                    case "202":
+                                        if (osaset.Length == 3)
+                                        {
+                                            Console.WriteLine("Vastustajasi on: " + osaset[2]);
+                                        }
+                                        Console.Write("Arvaa numero: ");
+                                        string arvaus = Console.ReadLine();
+                                        asiakas.SendTo(Encoding.UTF8.GetBytes("DATA" + erotin + arvaus), iep);
+                                        TILA = "GAME";
+                                        break;
+                                    case "203":
+                                        if (osaset.Length == 3)
+                                        {
+                                            Console.WriteLine("Vastustajasi on: " + osaset[2]);
+                                        }
+                                        Console.WriteLine("vastustajasi aloittaa, odota...");
+                                        TILA = "GAME";
+                                        break;
+                                    case "401":
+                                        Console.WriteLine("pelissä oli jo 2 pelaajaa!");
+                                        on = false;
+                                        break;
+                                    default:
+                                        Console.WriteLine("Ei ollut ACK 2xx!");
+                                        on = false;
+                                        break;
+                                }   //switch osaset 1
+                                break;
+                            default:
+                                Console.WriteLine("Ei ollut ACK !");
+                                on = false;
+                                break;
+                        }   //switch osaset 0
+                        break;
+                    case "GAME":
+                        switch (osaset[0])
+                        {
+                            case "ACK":
+                                if (osaset[1] == "407") //ei ollut numero!
+                                {
+                                    Console.Write("arvaa numero: ");
+                                    string uusi_arvaus = Console.ReadLine();
+                                    asiakas.SendTo(Encoding.UTF8.GetBytes("DATA" + erotin + uusi_arvaus), iep);
+                                }
+                                else if (osaset[1] != "300")
+                                {
+                                    Console.WriteLine("Ei ollut ACK 300 !");
+                                    on = false;
+                                }
+                                break;
+                            case "DATA":
+                                asiakas.SendTo(Encoding.UTF8.GetBytes("ACK" + erotin + "300"), iep);
+                                Console.Write("arvaa numero: ");
+                                string arvaus = Console.ReadLine();
+                                asiakas.SendTo(Encoding.UTF8.GetBytes("DATA" + erotin + arvaus), iep);
+                                break;
+                            case "QUIT":
+                                asiakas.SendTo(Encoding.UTF8.GetBytes("ACK" + erotin + "500"), iep);
+                                switch(osaset[1])
+                                {
+                                    case "501":
+                                        Console.WriteLine("Voitin! :D");
+                                        break;
+                                    case "502":
+                                        Console.WriteLine("Hävisit... :*(");
+                                        break;
+                                }//osaset 1
+                                if (osaset.Length == 3)
+                                {
+                                    Console.WriteLine(osaset[2]);
+                                }
+                                on = false;
+                                break;
+                            default:
+                                Console.WriteLine("Ei ollut ACK , DATA tai edes QUIT!");
+                                on = false;
+                                break;
+                        }//switch osaset 0
+                        break;
+
+                }   //switch TILA
+            }//while on
+
+            Console.ReadKey();
+            asiakas.Close();
+        }
+    }
+}
---
+++ PeliServeri.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+
+namespace Peliserveri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket palvelin = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            IPEndPoint iep = new IPEndPoint(IPAddress.Loopback, 9999);
+            palvelin.Bind(iep);
+            bool on = true;
+            string TILA = "WAIT";
+            int pelaajia = 0;
+            EndPoint[] pelaajat = new EndPoint[2];
+            string[] nimet = new string[2];
+            int vuorot = -1, luku = -1, kuittauksia = 0;
+            while(on)
+            {
+                byte[] rec = new byte[256];
+                IPEndPoint iap = new IPEndPoint(IPAddress.Any, 0);
+                EndPoint remoteEP = (EndPoint)iap;
+                int paljon = palvelin.ReceiveFrom(rec, ref remoteEP);
+
+                string viesti = Encoding.UTF8.GetString(rec, 0, paljon);
+                char[] erottimet = { ' ' };
+                string[] osaset = viesti.Split(erottimet, 3);
+                Console.WriteLine(viesti);
+
+                switch (TILA)    //vastaanotto
+                {
+                    case "WAIT":
+                        switch (osaset[0])
+                        {
+                            case "JOIN":
+                                nimet[pelaajia] = osaset[1];
+                                pelaajat[pelaajia] = remoteEP;
+                                if (pelaajia == 0)
+                                {
+                                    palvelin.SendTo(Encoding.UTF8.GetBytes("ACK 201"), pelaajat[pelaajia]);
+                                }
+                                else if(pelaajia == 1)
+                                {
+                                    Random rand = new Random();
+                                    int aloittaja = rand.Next(0,2);
+                                    vuorot = aloittaja;
+                                    luku = rand.Next(1,11);
+                                    palvelin.SendTo(Encoding.UTF8.GetBytes("ACK 202 " + nimet[1 - aloittaja]), pelaajat[aloittaja]);
+                                    palvelin.SendTo(Encoding.UTF8.GetBytes("ACK 203 " + nimet[aloittaja]), pelaajat[1 - aloittaja]);
+                                    TILA = "GAME";
+                                }
+                                pelaajia++;
+                                //else jotain pielessä
+                                break;
+                            default:
+                                Console.WriteLine("WAIT tilassa odotetaan JOIN kahdelta pelaajalta");
+                                break;
+                        }//switch (osaset[0])
+                        break;
+                    case "GAME":
+                        switch (osaset[0])
+                        {
+                            case "DATA":
+                                if (remoteEP.Equals(pelaajat[vuorot]))
+                                {
+                                    try
+                                    {
+                                        if (int.Parse(osaset[1]) == luku)
+                                        {
+                                            palvelin.SendTo(Encoding.UTF8.GetBytes("QUIT 501 " + "Voitit arvaamalla: " + osaset[1]), pelaajat[vuorot]);
+                                            palvelin.SendTo(Encoding.UTF8.GetBytes("QUIT 502 " + "Hävisit! Oikea luku oli: " + osaset[1]), pelaajat[1 - vuorot]);
+                                            TILA = "END";
+                                        }
+                                        else //arvasit väärin
+                                        {
+                                            palvelin.SendTo(Encoding.UTF8.GetBytes("ACK 300"), pelaajat[vuorot]);
+                                            palvelin.SendTo(Encoding.UTF8.GetBytes("DATA " + osaset[1]), pelaajat[1 - vuorot]);
+                                            vuorot = 1 - vuorot;
+                                            TILA = "WAIT_ACK";
+                                        }
+                                    }
+                                    catch
+                                    {
+                                        palvelin.SendTo(Encoding.UTF8.GetBytes("ACK 407 arvauksesi ei ole numero"), remoteEP);
+                                    }
+                                }
+                                else
+                                {
+                                    palvelin.SendTo(Encoding.UTF8.GetBytes("ACK 403 Ei ole vuorosi vielä"), remoteEP);
+                                }
+                                break;
+                            case "JOIN":
+                                palvelin.SendTo(Encoding.UTF8.GetBytes("ACK 401 Peli jo käynnissä"), remoteEP);
+                                break;
+                            default:
+                                Console.WriteLine("Ei ollut DATA viesti");
+                                break;
+                        }
+                        break;
+                    case "WAIT_ACK":
+                        if (remoteEP.Equals(pelaajat[vuorot]) && osaset[0] == "ACK" && osaset[1] == "300")
+                        {
+                            TILA = "GAME";
+                        }
+                        else
+                        {
+                            palvelin.SendTo(Encoding.UTF8.GetBytes("ACK 409 Odotetaan ACK 300 viestiä"), remoteEP);
+                        }
+                        break;
+                    case "END":
+                        if (osaset[0] == "ACK" && osaset[1] == "500")
+                        {
+                            kuittauksia++;
+                            if (kuittauksia == pelaajia)
+                            {
+                                on = false;
+                            }
+                        }
+                        break;
+                }//switch(TILA)
+            }//while(on)
+            Console.ReadKey();
+            palvelin.Close();
+        }
+    }
+}
